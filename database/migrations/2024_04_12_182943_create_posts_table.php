df5b66cd0557abd66b2d59e5eeed3b07bbc0dcd7
Donát Tasi <tasidonat@gmail.com>
2024-04-12 20:54:16 +0200

    Migartions

---
+++ database/migrations/2024_04_12_182943_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('category_id');
+            $table->integer('user_id');
+            $table->string('title');
+            $table->text('thumbnail')->nullable();
+            $table->text('content')->nullable();
+            $table->integer('status');
+            $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
